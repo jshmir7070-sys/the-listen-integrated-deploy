1abc1786ce1f00f12dd2b5172c5397dc82aba0a5
jshmir7070-sys <jshmir7070@gmail.com>
2025-11-05 17:03:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 정적 파일 제공
+// public 폴더 등록
 app.use(express.static(path.join(__dirname, "public")));
 
-// 기본 페이지 (로그인)
+// 기본 페이지 -> 로그인
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// 로그인 후 메인 이동
+// 로그인 → 메인 이동
 app.get("/main", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "main.html"));
 });
 
-// 서버 실행
+// 메인 → 지도 이동
+app.get("/map", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "map.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 The Listen 서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
